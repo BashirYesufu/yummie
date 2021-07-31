b076eb9e1152f107450ced79e1dd4e2965b3a9d3
Bashir Yesufu <yesufubashir@gmail.com>
2021-07-31 23:59:31 +0100

    Popular dishes collection view implemented

---
+++ Yummie/CustomViews/CardView/CardView.swift
@@ -0,0 +1,27 @@
+//
+//  CardView.swift
+//  Yummie
+//
+//  Created by mac on 7/31/21.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func initialSetUp() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.cornerRadius = 10
+        layer.shadowOpacity = 0.1
+        cornerRadius = 10
+    }
+}
--- Yummie/Views/Home/HomeViewController.swift
+++ Yummie/Views/Home/HomeViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var categoryCollectionView: UICollectionView!
+    @IBOutlet weak var popularDishesCollectionView: UICollectionView!
     
     var categories: [DishCategory] = [
         .init(id: "id!", name: "African Dish", image: "https://picsum.photos/100/200"),
             .init(id: "id!", name: "African Dish 2", image: "https://picsum.photos/100/200"),
             .init(id: "id!", name: "African Dish 3", image: "https://picsum.photos/100/200"),
             .init(id: "id!", name: "African Dish 4", image: "https://picsum.photos/100/200"),
-            .init(id: "id!", name: "African Dish 5", image: "https://picsum.photos/100/200")
+            .init(id: "id!", name: "African Dish 5", image: "https://picsum.photos/100/200"),
+                .init(id: "id!", name: "African Dish 6", image: "https://picsum.photos/100/200"),
+                .init(id: "id!", name: "African Dish 7", image: "https://picsum.photos/100/200"),
+                .init(id: "id!", name: "African Dish 8", image: "https://picsum.photos/100/200"),
+                .init(id: "id!", name: "African Dish 9", image: "https://picsum.photos/100/200"),
+                    .init(id: "id!", name: "African Dish 10", image: "https://picsum.photos/100/200")
     ]
     
     override func viewDidLoad() {
@@ -26,6 +32,8 @@ class HomeViewController: UIViewController {
 
     private func registerCells() {
         categoryCollectionView.register(UINib(nibName: CategoryCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
+        
+        popularDishesCollectionView.register(UINib(nibName: CategoryCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
     }
 }
 
@@ -37,6 +45,8 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.identifier, for: indexPath) as! CategoryCollectionViewCell
+        
+        cell.setup(category: categories[indexPath.row])
         return cell
     }
 }
